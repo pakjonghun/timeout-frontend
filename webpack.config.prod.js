d1087a6a7784505210338516e6e1397dac6ca1f3
정훈박 <fireking5997@gmail.com>
2022-06-22 11:07:42 +0900

    fix:process error

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const commonConfig = require('./webpack.config.common');
 const { merge } = require('webpack-merge');
-const webpack = require('webpack');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -16,12 +15,6 @@ module.exports = merge(commonConfig, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
-    }),
-    new webpack.ProvidePlugin({
-      // Make a global `process` variable that points to the `process` package,
-      // because the `util` package expects there to be a global variable named `process`.
-      // Thanks to https://stackoverflow.com/a/65018686/14239942
-      process: 'process/browser',
     }),
   ],
 });
